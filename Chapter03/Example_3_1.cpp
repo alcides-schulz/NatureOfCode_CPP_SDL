81ef74e04fa57ebd206b10c6450403499d1f8b0f
Alcides Schulz <alcides_schulz@hotmail.com>
2025-03-05 07:53:24 -0500

    adjust example 3.1 code.

--- Chapter03/Example_3_1.cpp
+++ Chapter03/Example_3_1.cpp
@@ -8,23 +8,25 @@ bool Example_3_1::UserRender(int elapsed_time)
     // Translate() function simulation
     int screen_x_center = WindowWidth() / 2;
     int screen_y_center = WindowHeight() / 2;
-    int line_x1 = screen_x_center - 100, line_y1 = screen_y_center - 100;
-    int line_x2 = screen_x_center + 100, line_y2 = screen_y_center + 100;
+    int line_x1 = screen_x_center - 100;
+    int line_y1 = screen_y_center - 100;
+    int line_x2 = screen_x_center + 100;
+    int line_y2 = screen_y_center + 100;
 
     // Rotate() function simulation
-    int centerX = (line_x1 + line_x2) / 2;
-    int centerY = (line_y1 + line_y2) / 2;
+    int line_center_x = (line_x1 + line_x2) / 2;
+    int line_center_y = (line_y1 + line_y2) / 2;
     double radians = angle_ * M_PI / 180.0;
-    int rotatedX1 = (int)(centerX + (line_x1 - centerX) * std::cos(radians) - (line_y1 - centerY) * std::sin(radians));
-    int rotatedY1 = (int)(centerY + (line_x1 - centerX) * std::sin(radians) + (line_y1 - centerY) * std::cos(radians));
-    int rotatedX2 = (int)(centerX + (line_x2 - centerX) * std::cos(radians) - (line_y2 - centerY) * std::sin(radians));
-    int rotatedY2 = (int)(centerY + (line_x2 - centerX) * std::sin(radians) + (line_y2 - centerY) * std::cos(radians));
+    int rotated_x1 = (int)(line_center_x + (line_x1 - line_center_x) * std::cos(radians) - (line_y1 - line_center_y) * std::sin(radians));
+    int rotated_y1 = (int)(line_center_y + (line_x1 - line_center_x) * std::sin(radians) + (line_y1 - line_center_y) * std::cos(radians));
+    int rotated_x2 = (int)(line_center_x + (line_x2 - line_center_x) * std::cos(radians) - (line_y2 - line_center_y) * std::sin(radians));
+    int rotated_y2 = (int)(line_center_y + (line_x2 - line_center_x) * std::sin(radians) + (line_y2 - line_center_y) * std::cos(radians));
 
     SDL_SetRenderDrawColor(Renderer(), 255, 0, 0, 255); // Red line
-    SDL_RenderDrawLine(Renderer(), rotatedX1, rotatedY1, rotatedX2, rotatedY2);
+    SDL_RenderDrawLine(Renderer(), rotated_x1, rotated_y1, rotated_x2, rotated_y2);
 
-    DrawCircle({ rotatedX1, rotatedY1 }, 5, { 255, 0, 0, 0 }, true);
-    DrawCircle({ rotatedX2, rotatedY2 }, 5, { 255, 0, 0, 0 }, true);
+    DrawCircle({ rotated_x1, rotated_y1 }, 5, { 255, 0, 0, 0 }, true);
+    DrawCircle({ rotated_x2, rotated_y2 }, 5, { 255, 0, 0, 0 }, true);
 
     angleVelocity_ += angleAcceleration_;
     angle_ += angleVelocity_;
